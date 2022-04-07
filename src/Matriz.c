9f4d9f8ca272815d80e845a2819597370b17755a
Pedro <pcampos952@gmail.com>
2022-04-07 11:47:55 -0300

    Edição 2 passada na 2º aula

--- src/Matriz.c
+++ src/Matriz.c
@@ -1,4 +1,3 @@
-
 #include "Matriz.h"
 
 void CriacaoMatriz(int nr_ordem, int **matriz)
@@ -28,82 +27,136 @@ void ImprimirMatriz(int nr_ordem, int **matriz)
 void PercorrendoMatriz(int nr_ordem, int **matriz)
 {
 
-    int i=0, j=0, soma_dos_valores=0;
-    bool invalida_celula=false;
-    soma_dos_valores+=matriz[i][j];
-    matriz[i][j]=invalida_celula;
+    int i = 0, j = 0, soma_dos_valores = 0, count = 0;
+    int coleta_valores[nr_ordem*nr_ordem];
+    bool invalida_celula = false;
+    coleta_valores[count] = matriz[i][j];
+    count++;
+    soma_dos_valores += matriz[i][j];
+    matriz[i][j] = invalida_celula;
 
-    while(i < nr_ordem && j < nr_ordem){
-        if(i < nr_ordem-1 && j == 0){ //borda lateral esquerda
-            if(matriz[i][j+1]>=matriz[i+1][j] && matriz[i][j+1]!=invalida_celula){
-                soma_dos_valores+=matriz[i][j+1];
-                matriz[i][j+1]=invalida_celula;
+    while (i < nr_ordem && j < nr_ordem)
+    {
+        if (i < nr_ordem - 1 && j == 0)
+        { // borda lateral esquerda
+            if (matriz[i][j + 1] >= matriz[i + 1][j] && matriz[i][j + 1] != invalida_celula)
+            {
+                soma_dos_valores += matriz[i][j + 1];
+                coleta_valores[count] = matriz[i][j + 1];
+                matriz[i][j + 1] = invalida_celula;
+                count++;
                 j++;
             }
-            else if(matriz[i+1][j]>=matriz[i][j+1] && matriz[i+1][j]!=invalida_celula){
-                soma_dos_valores+=matriz[i+1][j];
-                matriz[i+1][j]=invalida_celula;
-                i++;
-            }
-        }
-        else if(i==0 && j<nr_ordem-1){ //borda superior 
-            if(matriz[i][j+1]>=matriz[i+1][j] && matriz[i][j+1]!=invalida_celula){
-                soma_dos_valores+=matriz[i][j+1];
-                matriz[i][j+1]=invalida_celula;
-                j++;
-            }
-            else if(matriz[i + 1][j] >= matriz[i][j + 1] && matriz[i + 1][j] != invalida_celula)
+            else if (matriz[i + 1][j] >= matriz[i][j + 1] && matriz[i + 1][j] != invalida_celula)
             {
                 soma_dos_valores += matriz[i + 1][j];
+                coleta_valores[count] = matriz[i + 1][j];
                 matriz[i + 1][j] = invalida_celula;
+                count++;
                 i++;
             }
         }
-        else if(i==0 && j==nr_ordem){ //ponta da direita superior
+        else if (i == 0 && j < nr_ordem - 1)
+        { // borda superior
+            if (matriz[i][j + 1] >= matriz[i + 1][j] && matriz[i][j + 1] != invalida_celula)
+            {
+                soma_dos_valores += matriz[i][j + 1];
+                coleta_valores[count] = matriz[i][j + 1];
+                matriz[i][j + 1] = invalida_celula;
+                count++;
+                j++;
+            }
+            else if (matriz[i + 1][j] >= matriz[i][j + 1] && matriz[i + 1][j] != invalida_celula)
+            {
+                soma_dos_valores += matriz[i + 1][j];
+                coleta_valores[count] = matriz[i + 1][j];
+                matriz[i + 1][j] = invalida_celula;
+                count++;
+                i++;
+            }
+        }
+        else if (i == 0 && j == nr_ordem)
+        { // ponta da direita superior
             soma_dos_valores += matriz[i + 1][j];
+            coleta_valores[count] = matriz[i + 1][j];
             matriz[i + 1][j] = invalida_celula;
+            count++;
             i++;
         }
-        else if(i<nr_ordem-1 && j==nr_ordem-1){ //borda lateral direita (sem ponta)
-            if(matriz[i][j-1]>=matriz[i+1][j]&&matriz[i][j-1]!=invalida_celula){
-                soma_dos_valores+=matriz[i][j-1];
-                matriz[i][j-1]=invalida_celula;
+        else if (i < nr_ordem - 1 && j == nr_ordem - 1)
+        { // borda lateral direita (sem ponta)
+            if (matriz[i][j - 1] >= matriz[i + 1][j] && matriz[i][j - 1] != invalida_celula)
+            {
+                soma_dos_valores += matriz[i][j - 1];
+                coleta_valores[count] = matriz[i][j - 1];
+                matriz[i][j - 1] = invalida_celula;
+                count++;
                 j--;
             }
-            else if (matriz[i+1][j] >= matriz[i][j-1] && matriz[i+1][j] != invalida_celula){
+            else if (matriz[i + 1][j] >= matriz[i][j - 1] && matriz[i + 1][j] != invalida_celula)
+            {
                 soma_dos_valores += matriz[i + 1][j];
+                coleta_valores[count] = matriz[i + 1][j];
                 matriz[i + 1][j] = invalida_celula;
+                count++;
                 i++;
             }
         }
-        else if(i!=nr_ordem-1 && j!=nr_ordem-1){ //elementos que não estão na borda
+        else if (i != nr_ordem - 1 && j != nr_ordem - 1)
+        { // elementos que não estão na borda
             if (matriz[i][j + 1] > matriz[i + 1][j] && matriz[i][j + 1] > matriz[i][j - 1] && matriz[i][j + 1] != invalida_celula)
             {
                 soma_dos_valores += matriz[i][j + 1];
+                coleta_valores[count] = matriz[i][j + 1];
                 matriz[i][j + 1] = invalida_celula;
+                count++;
                 j++;
             }
             else if (matriz[i + 1][j] >= matriz[i][j - 1] && matriz[i + 1][j] > matriz[i][j + 1] && matriz[i + 1][j] != invalida_celula)
             {
                 soma_dos_valores += matriz[i + 1][j];
+                coleta_valores[count] = matriz[i + 1][j];
                 matriz[i + 1][j] = invalida_celula;
+                count++;
                 i++;
             }
-            else if (matriz[i][j - 1] >= matriz[i + 1][j] && matriz[i][j - 1] > matriz[i][j+1] && matriz[i][j - 1] != invalida_celula)
+            else if (matriz[i][j - 1] >= matriz[i + 1][j] && matriz[i][j - 1] > matriz[i][j + 1] && matriz[i][j - 1] != invalida_celula)
             {
                 soma_dos_valores += matriz[i][j - 1];
+                coleta_valores[count] = matriz[i][j - 1];
                 matriz[i][j - 1] = invalida_celula;
+                count++;
                 j--;
             }
         }
-        else if(i==nr_ordem-1 && j<nr_ordem-1){
+        else if (i == nr_ordem - 1 && j < nr_ordem - 1)
+        {
             soma_dos_valores += matriz[i][j + 1];
+            coleta_valores[count] = matriz[i][j + 1];
             matriz[i][j + 1] = invalida_celula;
+            count++;
             j++;
         }
-        else if(i==nr_ordem-1 && j==nr_ordem-1){
+        else if (i == nr_ordem - 1 && j == nr_ordem - 1)
+        {
             j++;
-        }   
+        }
     }
     printf("\n\nA soma dos valores percorridos é: %d\n", soma_dos_valores);
+
+    ImprimirMatriz(nr_ordem, matriz);
+    printf("\n");
+
+    printf("\nValores que foram somados (ordem normal): ");
+    for (i = 0; i < count; i++)
+    {
+        printf("%d ", coleta_valores[i]);
+    }
+
+    printf("\nValores que foram somados (ordem reversa): ");
+        for (i = count-1; i >= 0; i--)
+        {
+            printf("%d ", coleta_valores[i]);
+        }
+
 }
